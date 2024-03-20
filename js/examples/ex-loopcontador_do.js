e1a7e687f2b6adea76e8d6feeae6a9d835a049bf
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-03-20 15:19:41 -0500

    feat: :sparkles: loop do while
    a example for definition to use the loop do while and the difference with the loop while

---
+++ js/examples/ex-loopcontador_do.js
@@ -0,0 +1,9 @@
+// Ejemplo Practico para loop (do while)
+
+let contador = 0;
+do {
+    console.log(contador);
+    contador++;
+} while (contador < 10)
+
+// la diferencia con el while es que valida antes de ejecutar el while
