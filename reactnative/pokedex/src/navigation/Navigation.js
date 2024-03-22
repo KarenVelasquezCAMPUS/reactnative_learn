bae6f200c962064d1093c5cfe021f9a7db9e3cc9
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-03-22 17:37:06 -0500

    perf: :alien: se obtuvo con exito la info de la api
    se consume la pokeap y se logran obtener los datos pedidos segun con lo que retorna la api

--- reactnative/pokedex/src/navigation/Navigation.js
+++ reactnative/pokedex/src/navigation/Navigation.js
@@ -3,7 +3,8 @@ import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from "react-native-vector-icons/FontAwesome5"
 import FavoriteScreen from '../screens/Favorite';
-import PokedexScreen from '../screens/Pokedex';
+/* import PokedexScreen from '../screens/Pokedex'; */
+import PokedexNavigation from './PokedexNavigation';
 import Account from '../screens/Account';
 
 const Tab = createBottomTabNavigator();
@@ -11,20 +12,37 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name = 'Favorite' component = {FavoriteScreen} options={{
+            <Tab.Screen 
+            name = 'Favorite' 
+            component = {FavoriteScreen} 
+            options={{
                 tabBarLabel: "Favoritos",
+                headerTitle: "Favoritos",
+                headerTitleAlign: 'center',
                 tabBarIcon: ({ color, size }) => {
                     return (
                         <Icon name='heart' color= {color} size={size} />
                     )
+
                 }
             }} />
-            <Tab.Screen name = 'Pokedex' component = {PokedexScreen} options={{
+            <Tab.Screen 
+            name = 'Pokedex' 
+            component = {PokedexNavigation} 
+            options={{
                 tabBarLabel: "",
+                headerTitle: "Pokedex",
+                headerTitleAlign: 'center',
+                headerShown: false,
                 tabBarIcon: () => renderPokeball(),
             }} />
-            <Tab.Screen name = 'Account' component = {Account} options={{
+            <Tab.Screen 
+            name = 'Account' 
+            component = {Account} 
+            options={{
                 tabBarLabel: "Mi cuenta",
+                headerTitle: "Mi cuenta",
+                headerTitleAlign: 'center',
                 tabBarIcon: ({ color, size }) => {
                     return (
                         <Icon name="user" color= {color} size= {size} />
--- reactnative/pokedex/src/screens/Pokedex.js
+++ reactnative/pokedex/src/screens/Pokedex.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { getPokemonDetailsByUrlApi, getPokemonsApi } from '../api/pokemon';
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log("pokemons----->", pokemons);
+
+  useEffect(() => {
+    (async() => {
+      await loadPokemons();
+    })();
+  }, []);
+  
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          imagen: pokemonDetails.sprites.other['official-artwork'].front_default,
+        });
+      }
+
+      setPokemons([...pokemons, ...pokemonsArray]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View>
-      <Text>Pokedex</Text>
+      <Text>Pokedex Principal</Text>
     </View>
-  )
+  );
 }
