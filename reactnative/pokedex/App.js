b47ebfced7e97ac6472b9f1b638d1c7b548e960b
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-03-22 15:38:10 -0500

    feat: :hammer: comienzo de pokedex
    se dio inicio a el pimer proyecto completo en react native de la pokedex, se hizo la creacion del proyecto, instalacion de dependencias y se hizo un tab menu (un menu inferior)

---
+++ reactnative/pokedex/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './src/navigation/Navigation';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+  );
+}
---
+++ reactnative/pokedex/src/navigation/Navigation.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5"
+import FavoriteScreen from '../screens/Favorite';
+import PokedexScreen from '../screens/Pokedex';
+import Account from '../screens/Account';
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name = 'Favorite' component = {FavoriteScreen} options={{
+                tabBarLabel: "Favoritos",
+                tabBarIcon: ({ color, size }) => {
+                    return (
+                        <Icon name='heart' color= {color} size={size} />
+                    )
+                }
+            }} />
+            <Tab.Screen name = 'Pokedex' component = {PokedexScreen} options={{
+                tabBarLabel: "",
+                tabBarIcon: () => renderPokeball(),
+            }} />
+            <Tab.Screen name = 'Account' component = {Account} options={{
+                tabBarLabel: "Mi cuenta",
+                tabBarIcon: ({ color, size }) => {
+                    return (
+                        <Icon name="user" color= {color} size= {size} />
+                    )
+                }
+            }} />
+        </Tab.Navigator>
+    );
+}
+
+function renderPokeball () {
+    return (
+        <Image 
+            source = {require('../assets/pokeball.png')}
+            style = {{ width: 75, height: 75, top: -15 }}
+        />
+    )
+}
