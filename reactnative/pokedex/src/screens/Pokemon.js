45f9ccb0d26296407d68d50043464cb8796d3219
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-03-26 15:36:33 -0500

    feat: :technologist: consulta detalles pokemon

--- reactnative/pokedex/src/screens/Pokemon.js
+++ reactnative/pokedex/src/screens/Pokemon.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect} from 'react';
+import { View, Text } from 'react-native';
+import { getPokemonDetailsApi } from '../api/pokemon';
 
 export default function Pokemon(props) {
-  const { navigation, route } = props;
-console.log(props);
+  const { navigation, route: {params} } = props;
+  const [pokemon, setPokemon] = useState(null);
+  console.log(params.id);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id)
+        setPokemon(response);
+      } catch (error)  {
+        navigation.goBack();
+      }
+    }) ()
+  }, [params]);
+
+  if (!pokemon) return null;
 
   return (
     <View>
       <Text>Estamos en Pokemon</Text>
+      <Text>{pokemon.name}</Text>
     </View>
   )
 }
