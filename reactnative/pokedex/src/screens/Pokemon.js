42f1980f2cdeb0d16dbf2f62956cd86840e81277
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-03-26 17:42:42 -0500

    feat: :sparkles: maquetacion
    seccion de tipos

--- reactnative/pokedex/src/screens/Pokemon.js
+++ reactnative/pokedex/src/screens/Pokemon.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 import { ScrollView } from 'react-native';
 import { getPokemonDetailsApi } from '../api/pokemon';
 import Header from '../components/Pokemon/Header';
+import Type from '../components/Pokemon/Type';
 
 export default function Pokemon(props) {
   const { navigation, route: {params} } = props;
@@ -24,6 +25,7 @@ export default function Pokemon(props) {
   return (
     <ScrollView>
       <Header name = {pokemon.name} order = {pokemon.order} image = {pokemon.sprites.other['official-artwork'].front_default} type = {pokemon.types[0].type.name} />
+      <Type types = {pokemon.types} />
     </ScrollView>
   )
 }
