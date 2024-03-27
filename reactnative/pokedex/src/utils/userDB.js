940d9b946f478ecdd387abf4b7ab83c1f3bcf991
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-03-27 09:06:24 -0500

    feat: :technologist: creacion basica de formulario login

---
+++ reactnative/pokedex/src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+    usrename: "zkfps05",
+    password: "10150325"
+};
+
+export const userDetails = {
+    username: "zkfps05",
+    firstName: "Karen",
+    lastName: "Velasquez",
+    email: "karenvelasquez1009@gmail.com"
+}
