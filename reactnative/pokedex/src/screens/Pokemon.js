be545efebb078533438fad2da1e48ae0e62abf93
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-03-27 08:16:41 -0500

    feat: :technologist: estadisticas
    se realizo estadistica segun stat de la api en barras

--- reactnative/pokedex/src/screens/Pokemon.js
+++ reactnative/pokedex/src/screens/Pokemon.js
@@ -3,6 +3,7 @@ import { ScrollView } from 'react-native';
 import { getPokemonDetailsApi } from '../api/pokemon';
 import Header from '../components/Pokemon/Header';
 import Type from '../components/Pokemon/Type';
+import Stats from '../components/Pokemon/Stats';
 
 export default function Pokemon(props) {
   const { navigation, route: {params} } = props;
@@ -26,6 +27,7 @@ export default function Pokemon(props) {
     <ScrollView>
       <Header name = {pokemon.name} order = {pokemon.order} image = {pokemon.sprites.other['official-artwork'].front_default} type = {pokemon.types[0].type.name} />
       <Type types = {pokemon.types} />
+      <Stats stats = {pokemon.stats} />
     </ScrollView>
   )
 }
