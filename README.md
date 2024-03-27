6d931f862b400bf4cf997e30e2a69aefbf489ca7
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-03-27 10:02:57 -0500

    feat: :construction: formularios avanzados
    dependencia formik y yup

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Project created to store a documentation and application of what is basic javasc
 
 (instale npm install lodash, en pokedex para estilos)
 (navigation.navigate solo recibe texto planos, no recibe funciones ni objetos)
+(formik para formulario, yup sistema de validacion de formulario)
--- reactnative/pokedex/src/components/Auth/LoginForm.js
+++ reactnative/pokedex/src/components/Auth/LoginForm.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, Button, Keyboard } from 'react-native';
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function LoginForm() {
+
+    const formik = useFormik({
+
+        initialValues: initialValues(),
+
+        validationSchema: Yup.object(validationSchema()),
+        validateOnChange: false,
+
+        onSubmit: (formValue) => {
+            console.log("Formulario enviado....");
+            console.log(formValue);
+        },
+    });
+
   return (
     <View>
         <Text style = {styles.title}>
             Iniciar sesión
         </Text>
-        <TextInput placeholder = 'Nombre de usuario' style = {styles.input} autoCapitalize = 'none' />
-        <TextInput placeholder='Contraseña' style = {styles.input} autoCapitalize = 'none' secureTextEntry = {true} />
-        <Button title = "Entrar" onPress = {() => console.log("Entrando....")} style = {styles.button} />
+        <TextInput placeholder = 'Nombre de usuario' style = {styles.input} autoCapitalize = 'none' value = {formik.values.username} onChangeText = {(text) => formik.setFieldValue("username", text)}  />
+        <TextInput placeholder='Contraseña' style = {styles.input} autoCapitalize = 'none' secureTextEntry = {true} value = {formik.values.password} onChangeText = {(text) => formik.setFieldValue("password", text)} />
+        <Button title = "Entrar" onPress = {formik.handleSubmit} style = {styles.button} />
+        <Text style = {styles.error}>
+            {formik.errors.username}
+        </Text>
+        <Text style = {styles.error}>
+            {formik.errors.password}
+        </Text>
     </View>
   )
+}
+
+function initialValues () {
+    return {
+        username: "",
+        password: ""
+    };
+}
+
+function validationSchema() {
+    return {
+        username: Yup.string().required("El usuario es obligatorio"),
+        password: Yup.string().required("La contraseña es obligatoria")
+    }
 }
 
 const styles = StyleSheet.create ({
@@ -28,5 +64,10 @@ const styles = StyleSheet.create ({
         borderWidth: 1,
         padding: 10,
         borderRadius: 10
+    },
+    error: {
+        textAlign: "center",
+        color: "#f00",
+        marginTop: 20
     }
 })
