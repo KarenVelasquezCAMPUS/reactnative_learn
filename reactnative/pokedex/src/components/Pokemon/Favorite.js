47c9266e0da48a29b2757e9672f00c9df5731e3c
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-04-01 09:26:57 -0500

    feat: :lipstick: icono favorito funcionalidad

--- reactnative/pokedex/src/components/Pokemon/Favorite.js
+++ reactnative/pokedex/src/components/Pokemon/Favorite.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
-import Icon from "react-native-vector-icons/FontAwesome5";
-import { addPokemonFavoriteApi } from '../../api/favorite';
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { addPokemonFavoriteApi, isPokemonFavoriteApi } from '../../api/favorite';
 
 export default function Favorite(props) {
 
     const { id } = props;
+    const [isFavorite, setIsFavorite] = useState(undefined);
+    const Icon = isFavorite ? FontAwesome : FontAwesome5;
+
+    useEffect (() => {
+      (async () => {
+        try {
+          const response = await isPokemonFavoriteApi(id);
+          setIsFavorite(response);
+        } catch (error) {
+          setIsFavorite(false);
+        }
+      }) ()
+    }, [id])
 
     const addFavorite = async () => {
       await addPokemonFavoriteApi(id);
     };
 
+    const removeFavorite = () => {
+      console.log("Eliminar de favoritos");
+    }
+
   return (
-    <Icon name = "heart" color = "#fff" onPress = {addFavorite} style = {{ marginRight: 20 }}  />
+    <Icon name = "heart" color = "#fff" onPress = {isFavorite ? removeFavorite : addFavorite} style = {{ marginRight: 20 }}  />
   );
 }
--- reactnative/pokedex/src/screens/Favorite.js
+++ reactnative/pokedex/src/screens/Favorite.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-import { getPokemonFavoriteApi } from '../api/favorite';
+import { getPokemonFavoriteApi, isPokemonFavoriteApi } from '../api/favorite';
 import { initial } from 'lodash';
 
 export default function Favorite() {
