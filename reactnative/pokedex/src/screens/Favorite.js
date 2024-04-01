4594ec4b39334aa19aa5f5436698b485334338bb
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2024-04-01 11:21:48 -0500

    feat: :construction: sistema de favoritos act
    se aplico la funcionalidad de actualizar a tiempo real si el usuario agrega un pokemon más a favoritos que no tenga que recargar para que cuando se dirija al apartado de favoritos este en timepo real filtre el nuevo seleccionado

--- reactnative/pokedex/src/screens/Favorite.js
+++ reactnative/pokedex/src/screens/Favorite.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, Text, Button } from 'react-native';
+import { useFocusEffect, userFocusEffect } from "@react-navigation/native";
 import { getPokemonFavoriteApi, isPokemonFavoriteApi } from '../api/favorite';
+import { getPokemonDetailsApi } from "../api/pokemon";
 import { initial } from 'lodash';
+import useAuth from "../hooks/useAuth";
+import PokemonList from "../components/PokemonList";
 
 export default function Favorite() {
 
-  const checkFavorites = async () => {
-    const response = await getPokemonFavoriteApi();
-    console.log(response);
-  }
+  const [pokemons, setPokemons] = useState([]);
+  const { auth } = useAuth();
+
+  useFocusEffect (
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          const response = await getPokemonFavoriteApi();
+  
+          const pokemonsArray = [];
+          for await (const id of response) {
+              const pokemonDetails = await getPokemonDetailsApi(id);
+              pokemonsArray.push({
+                  id: pokemonDetails.id,
+                  name: pokemonDetails.name,
+                  type: pokemonDetails.types[0].type.name,
+                  order: pokemonDetails.order,
+                  image: pokemonDetails.sprites.other['official-artwork'].front_default,
+              });
+          }
+          setPokemons(pokemonsArray);
+        }) ();
+      }
+    }, [auth])
+  )  
 
   return (
-    <View>
-      <Text>Favorite</Text>
-      <Button title = "Obtener Favoritos" onPress={checkFavorites} />
-    </View>
+    !auth ? <Text> Usuario no logeado </Text> : <PokemonList pokemons = {pokemons} />
   )
 }
